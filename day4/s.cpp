80a17195fdb6146b4e6be17c142a117c7c99d1c7
CalebBunch <rcbcg123@gmail.com>
2024-12-04 11:57:51 -0500

    day4 part2 done

--- day4/s.cpp
+++ day4/s.cpp
@@ -29,6 +29,10 @@ vector<string> splitString(string str, char splitter) {
     return result;
 }
 
+bool inBounds(int r, int c, int aRow, int aCol) {
+    return r >= 0 && r < aRow && c >= 0 && c < aCol;
+}
+
 int helper1(int row, int col, vector<vector<char>> a) {
     int res = 0;
 
@@ -42,7 +46,7 @@ int helper1(int row, int col, vector<vector<char>> a) {
         while (i < 3) {
             newRow += dir.first;
             newCol += dir.second;
-            if (newRow >= 0 && newRow < a.size() && newCol >= 0 && newCol < a[0].size()) {
+            if (inBounds(newRow, newCol, a.size(), a[0].size())) {
                 if (i == 0 && a[newRow][newCol] != 'M') {
                     f = false;
                     break;
@@ -81,6 +85,53 @@ int part1(vector<vector<char>> a) {
     return s;
 }
 
+int helper2(int row, int col, vector<vector<char>> &a) {
+    int res = 0;
+    char tl, br, tr, bl;
+    
+    if (inBounds(row - 1, col - 1, a.size(), a[0].size())) {
+        tl = a[row - 1][col - 1];
+    } else {
+        return 0;
+    }
+    if (inBounds(row + 1, col + 1, a.size(), a[0].size())) {
+        br = a[row + 1][col + 1];
+    } else {
+        return 0;
+    }
+    if (inBounds(row - 1, col + 1, a.size(), a[0].size())) {
+        tr = a[row - 1][col + 1]; 
+    } else {
+        return 0;
+    }
+    if (inBounds(row + 1, col - 1, a.size(), a[0].size())) {
+        bl = a[row + 1][col - 1];
+    } else {
+        return 0;
+    }
+
+    bool cond1 = ( (tl == 'M' || tl == 'S') && (br == 'M' || br == 'S') && tl != br );
+    bool cond2 = ( (tr == 'M' || tr == 'S') && (bl == 'M' || bl == 'S') && tr != bl );
+    
+    if (cond1 && cond2) {
+        return 1; 
+    }
+
+    return 0;
+}
+
+int part2(vector<vector<char>> a) {
+    int s = 0;
+    for (int i = 1; i < a.size() - 1; i++) { 
+        for (int j = 1; j < a[i].size() - 1; j++) {
+            if (a[i][j] == 'A') {
+                s += helper2(i, j, a);
+            }
+        }
+    }
+    return s;
+}
+
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
@@ -103,6 +154,7 @@ int main() {
     }
 
     cout << "Part 1: " << part1(d) << endl;
+    cout << "Part 2: " << part2(d) << endl;
 
     return 0;
 }
