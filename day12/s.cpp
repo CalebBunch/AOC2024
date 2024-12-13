dc04c4f47e8a689fb725ef6d115ce43229d58507
CalebBunch <rcbcg123@gmail.com>
2024-12-12 19:29:33 -0500

    day 12 part 2 complete

--- day12/s.cpp
+++ day12/s.cpp
@@ -75,6 +75,103 @@ long long part1(vector<vector<char>>& a) {
     return s;
 }
 
+int dfs2(char ochar, int r, int c, vector<vector<char>> a, unordered_set<pair<int, int>, pair_hash>& explored, unordered_set<pair<int, int>, pair_hash>& area) {
+    pair<int, int> dirs[] = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+    pair<int, int> pos = make_pair(r, c);
+    int res = 0;
+    if (explored.find(pos) != explored.end() || !inBounds(r, c, a.size(), a[0].size())) {
+        return res;
+    }
+    if (a[r][c] != ochar) {
+        return res;
+    }
+    explored.insert(pos);
+    area.insert(pos);
+    res++;
+    for (auto dir : dirs) {
+        int nr = r + dir.first;
+        int nc = c + dir.second;
+        if (inBounds(nr, nc, a.size(), a[0].size()) && a[nr][nc] == ochar) {
+            res += dfs2(ochar, nr, nc, a, explored, area);
+        }
+    }
+    return res;
+}
+
+int calculate_sides(char ochar, unordered_set<pair<int, int>, pair_hash>& perimeter, vector<vector<char>>& a) {
+    int res = 0;
+    for (auto p : perimeter) {
+        int r = p.first;
+        int c = p.second;
+        
+        bool left = !inBounds(r, c - 1, a.size(), a[0].size()) || a[r][c - 1] != ochar;
+        bool right = !inBounds(r, c + 1, a.size(), a[0].size()) || a[r][c + 1] != ochar;
+        bool up = !inBounds(r - 1, c, a.size(), a[0].size()) || a[r - 1][c] != ochar;
+        bool down = !inBounds(r + 1, c, a.size(), a[0].size()) || a[r + 1][c] != ochar;
+
+        bool upleft = !inBounds(r - 1, c - 1, a.size(), a[0].size()) || a[r - 1][c - 1] != ochar;
+        bool upright = !inBounds(r - 1, c + 1, a.size(), a[0].size()) || a[r - 1][c + 1] != ochar;
+        bool downleft = !inBounds(r + 1, c - 1, a.size(), a[0].size()) || a[r + 1][c - 1] != ochar;
+        bool downright = !inBounds(r + 1, c + 1, a.size(), a[0].size()) || a[r + 1][c + 1] != ochar;
+
+        if (left && up && !upleft) {
+            res++;
+        }
+        if (right && up && !upright) {
+            res++;
+        }
+        if (left && down && !downleft) {
+            res++;
+        }
+        if (right && down && !downright) {
+            res++;
+        }
+
+        if (!left && !up && upleft) {
+            res++;
+        }
+        if (!right && !up && upright) {
+            res++;
+        }
+        if (!left && !down && downleft) {
+            res++;
+        }
+        if (!right && !down && downright) {
+            res++;
+        }
+
+        if (left && up && upleft) {
+            res++;
+        }
+        if (right && up && upright) {
+            res++;
+        }
+        if (left && down && downleft) {
+            res++;
+        }
+        if (right && down && downright) {
+            res++;
+        }
+    }
+
+    return res;
+}
+
+long long part2(vector<vector<char>>& a) {
+    long long s = 0;
+    unordered_set<pair<int, int>, pair_hash> explored;
+    for (int i = 0; i < a.size(); i++) {
+        for (int j = 0; j < a[i].size(); j++) {
+            pair<int, int> p = make_pair(i, j);
+            if (explored.find(p) == explored.end()) {
+                unordered_set<pair<int, int>, pair_hash> area;
+                s += (dfs2(a[i][j], i, j, a, explored, area) * calculate_sides(a[i][j], area, a));
+            }
+        }
+    }
+    return s;
+}
+
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
@@ -97,6 +194,7 @@ int main() {
     }
 
     cout << "Part 1: " << part1(d) << endl;
+    cout << "Part 2: " << part2(d) << endl;
 
     return 0;
 }
