3f6ba0558da791be943d50fa69f571929dcfff00
CalebBunch <rcbcg123@gmail.com>
2024-12-20 17:56:22 -0500

    day20 part2 done

--- day20/s.cpp
+++ day20/s.cpp
@@ -34,12 +34,16 @@ bool inBounds(int r, int c, int aRow, int aCol) {
     return r >= 0 && r < aRow && c >= 0 && c < aCol;
 }
 
-int dijkstra(pair<int, int> start, pair<int, int> end, vector<vector<char>> grid) {
+pair<int, vector<pair<int, int>>> dijkstra(pair<int, int> start, pair<int, int> end, vector<vector<char>> grid) {
     pair<int, int> dirs[] = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
     unordered_map<pair<int, int>, int, pair_hash> dist;
+    unordered_map<pair<int, int>, pair<int, int>, pair_hash> parent; 
     priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<>> pq;
+
     pq.push({0, start});
     dist[start] = 0;
+    parent[start] = start; 
+
     while (!pq.empty()) {
         pair<int, pair<int, int>> curr = pq.top();
         int current_dist = curr.first;
@@ -47,8 +51,17 @@ int dijkstra(pair<int, int> start, pair<int, int> end, vector<vector<char>> grid
         pq.pop();
 
         if (vertex == end) {
-            return current_dist;
+            vector<pair<int, int>> path;
+            pair<int, int> current = end;
+            while (current != start) {
+                path.push_back(current);
+                current = parent[current];
+            }
+            path.push_back(start);
+            reverse(path.begin(), path.end());
+            return {current_dist, path};
         }
+
         for (auto d : dirs) {
             int nr = vertex.first + d.first;
             int nc = vertex.second + d.second;
@@ -57,20 +70,19 @@ int dijkstra(pair<int, int> start, pair<int, int> end, vector<vector<char>> grid
             }
             if (dist.find({nr, nc}) == dist.end() || current_dist + 1 < dist[{nr, nc}]) {
                 dist[{nr, nc}] = current_dist + 1;
+                parent[{nr, nc}] = vertex;
                 pq.push({current_dist + 1, {nr, nc}});
             }
         }
     }
 
-    return -1;
+    return {-1, {}};
 }
 
-
-int part1(vector<vector<char>> grid) {
+int helper(vector<vector<char>> grid, int depth) {
     int s = 0;
     pair<int, int> start;
     pair<int, int> end;
-
     for (int i = 0; i < grid.size(); i++) {
         for (int j = 0; j < grid[i].size(); j++) {
             if (grid[i][j] == 'S') {
@@ -80,23 +92,27 @@ int part1(vector<vector<char>> grid) {
             }
         }
     }
-
-    int original_dist = dijkstra(start, end, grid);
-    
-    for (int i = 0; i < grid.size(); i++) {
-        for (int j = 0; j < grid[i].size(); j++) {
-            if (grid[i][j] == '#') {
-                grid[i][j] = '.';
-                int dist = dijkstra(start, end, grid);
-                if ((original_dist -  dist) >= 100) {
-                    s++;
-                }
-                grid[i][j] = '#';
-            }
+    pair<int, vector<pair<int, int>>> original = dijkstra(start, end, grid);
+    vector<pair<int, int>> path = original.second;
+    for (int i = 0; i < path.size(); i++) {
+        for (int j = i + 1; j < path.size(); j++) {
+             pair<int, int> p1 = path[i];
+             pair<int, int> p2 = path[j];
+             int mdist = abs(p1.first - p2.first) + abs(p1.second - p2.second);
+             if (mdist <= depth && j - i - mdist >= 100) {
+                s++;
+             }
         }
     }
-
     return s;
+}
+
+int part1(vector<vector<char>> grid) {
+    return helper(grid, 2);
+}
+
+int part2(vector<vector<char>> grid) {
+    return helper(grid, 20);
 }
 
 int main() {
@@ -121,6 +137,7 @@ int main() {
     }
 
     cout << "Part 1: " << part1(grid) << endl;
+    cout << "Part 2: " << part2(grid) << endl;
 
     return 0;
 }
