9daae137e519a86a7389eabbcd05c77b69e48c20
CalebBunch <rcbcg123@gmail.com>
2024-12-24 23:28:58 -0500

    day24 part 2 done

--- day24/s.cpp
+++ day24/s.cpp
@@ -118,13 +118,93 @@ unsigned long long part1(unordered_map<string, tuple<int, string, string, string
     return final_result;
 }
 
+void print_gate(string curr, unordered_map<string, tuple<int, string, string, string, int>> gates, int depth, int mdepth) {
+    if (depth > mdepth) {
+        return;
+    }
+    for (int i = 0; i < depth; i++) {
+        cout << " ";
+    }
+    string gate1 = get<1>(gates[curr]);
+    string gate2 = get<3>(gates[curr]);
+    cout << curr << " = " << gate1 << " " << get<2>(gates[curr]) << " " << gate2 << endl;
+    if (gate1[0] != 'x' && gate1[0] != 'y') {
+        print_gate(gate1, gates, depth + 1, mdepth);
+    }
+    if (gate2[0] != 'x' && gate2[0] != 'y') {
+        print_gate(gate2, gates, depth + 1, mdepth);
+    }
+}
+
+vector<string> part2(unordered_map<string, tuple<int, string, string, string, int>> gates) {
+    // solved by inspection
+    vector<string> result = {"z09", "cwt", "jmv", "css", "gdd", "z05", "z37", "pqt"};
+    sort(result.begin(), result.end());
+    return result;
+}
+
+void debug(unordered_map<string, tuple<int, string, string, string, int>> gates) {
+    /*
+    auto temp = gates["z09"];
+    gates["z09"] = gates["cwt"];
+    gates["cwt"] = temp;
+
+    auto temp2 = gates["jmv"];
+    gates["jmv"] = gates["css"];
+    gates["css"] = temp2;
+
+    auto temp3 = gates["gdd"];
+    gates["gdd"] = gates["z05"];
+    gates["z05"] = temp3;
+
+    auto temp4 = gates["z37"];
+    gates["z37"] = gates["pqt"];
+    gates["pqt"] = temp4;
+    */
+
+    for (unsigned long long x = 0; x < 45; x++) {
+        for (unsigned long long y = 0; y < 45; y++) {
+            unsigned long long num1 = (1ULL << x);
+            unsigned long long num2 = (1ULL << y);
+            bitset x_bits = bitset<45>(num1);
+            bitset y_bits = bitset<45>(num2);
+            bitset x_y_bits = bitset<45>(num1 + num2);
+            for (int i = 0; i < x_bits.size(); i++) {
+                if (i < 10) {
+                    get<4>(gates["x0" + to_string(i)]) = x_bits[i];
+                } else {
+                    get<4>(gates["x" + to_string(i)]) = x_bits[i];
+                }
+            }
+            for (int i = 0; i < y_bits.size(); i++) {
+                if (i < 10) {
+                    get<4>(gates["y0" + to_string(i)]) = y_bits[i];
+                } else {
+                    get<4>(gates["y" + to_string(i)]) = y_bits[i];
+                }
+            }
+            unsigned long long res = part1(gates);
+            if (res != (num1 + num2)) {
+                cout << "\ncircuit failed: got " << res << " but expected " << num1 + num2 << endl;
+                cout << "circuit failed at bit x = " << x << " and bit y = " << y << endl;
+                cout << "The following are relevant gates: \n\n";
+                string z_gate = "z";
+                if (y < 10) {
+                    z_gate += "0";
+                }
+                z_gate += to_string(y);
+                print_gate(z_gate, gates, 0, 5);
+            }
+        }
+    }
+}
+
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
 
     unordered_map<string, tuple<int, string, string, string, int>> gates;
     bool flag = false;
-
 
     ifstream file("input.txt");
     if (file.is_open()) {
@@ -148,5 +228,15 @@ int main() {
     }
 
     cout << "Part 1: " << part1(gates) << endl;
+    vector<string> res = part2(gates);
+    cout << "Part 2: ";
+    for (int i = 0; i < res.size(); i++) {
+        if (i == res.size() - 1) {
+            cout << res[i] << endl;
+        } else {
+            cout << res[i] << ",";
+        }
+    }
+
     return 0;
 }
